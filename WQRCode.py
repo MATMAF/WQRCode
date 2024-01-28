b64a30be8251da22f56b7f96130019bbb74a8b13
Mathieu Mafille <81818115+MATMAF@users.noreply.github.com>
2024-01-28 17:49:29 +0100

    Update WQRCode.py

--- WQRCode.py
+++ WQRCode.py
@@ -1,9 +1,7 @@
 import qrcode
 from flask import Flask, request, render_template
-from flask_sslify import SSLify
 
 app = Flask(__name__)
-sslify = SSLify(app)
 
 @app.route('/')
 def index():
@@ -22,4 +20,4 @@ def generate_qr():
     return render_template('qr_generated.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
